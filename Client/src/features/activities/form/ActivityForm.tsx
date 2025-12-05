ed6a6f62845fbd3f18e8874fee2db47f8d249252
Hassan <h.jamil1713@gmail.com>
2025-12-05 16:13:30 +0300

    End of Section 11

--- Client/src/features/activities/form/ActivityForm.tsx
+++ Client/src/features/activities/form/ActivityForm.tsx
@@ -1,35 +1,56 @@
-import { Box, Button, Paper, TextField, Typography } from '@mui/material'
-import type { FormEvent } from 'react';
+import { Box, Button, Paper, Typography } from '@mui/material'
+import { useEffect} from 'react';
 import { useActivities } from '../../../lib/hooks/useActivities';
 import { useNavigate, useParams } from 'react-router';
+import {useForm, type FieldValues} from 'react-hook-form';
+import { activitySchema, type ActivitySchema } from '../../../lib/schemas/activitySchema';
+import {zodResolver} from '@hookform/resolvers/zod';
+import TextInput from '../../../app/shared/components/TextInput';
+import SelectInput from '../../../app/shared/components/SelectInput';
+import { categoryOptions } from './categoryOptions';
+import DateTimeInput from '../../../app/shared/components/DateTimeInput';
+import LocationInput from '../../../app/shared/components/LocationInput';
 
 export default function ActivityForm() {
 
+  const  { reset, control, handleSubmit } = useForm<ActivitySchema>({
+    mode: 'onTouched',
+    resolver: zodResolver(activitySchema)
+  });
   const {id} = useParams();
   const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
   const navigate = useNavigate();
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    
-    const formData = new FormData(event.currentTarget);
-
-    const data: { [key: string]: FormDataEntryValue } = {};
-    formData.forEach((value, key) => {
-      data[key] = value;
+  useEffect(() => {
+    if(activity) reset({
+      ...activity,
+      location: {
+        city: activity.city,
+        venue: activity.venue,
+        latitude: activity.latitude,
+        longitude: activity.longitude
+      }
     });
+  },[activity, reset]);
 
-    if (activity) {
-      data.id = activity.id;
-      await updateActivity.mutateAsync(data as unknown as Activity);
-      navigate(`/activities/${activity.id}`);
-    }
-    else{
-      createActivity.mutate(data as unknown as Activity, {
-        onSuccess: (id) => {
-          navigate(`/activities/${id}`);
-        }
-      });
+  const onSubmit = async (data: FieldValues) => {
+    const {location, ...rest} = data;
+
+    const flattenedData = {...rest, ...location};
+
+    try {
+      if(activity){
+        updateActivity.mutate({...activity, ...flattenedData}, {
+          onSuccess: () => navigate(`/activities/${activity.id}`)
+        })
+      }
+      else{
+        createActivity.mutate(flattenedData, {
+          onSuccess: (id) => navigate(`/activities/${id}`)
+        })
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -38,14 +59,14 @@ export default function ActivityForm() {
   return (
     <Paper sx={{borderRadius: 3, padding: 3}}>
         <Typography variant='h5' gutterBottom color='primary'>{activity ? 'Editing' : 'Create'} Activity</Typography>
-        <Box component="form" onSubmit={handleSubmit} display="flex" flexDirection="column" gap={3}>
-            <TextField name='title' label='Title' defaultValue={activity?.title}/>
-            <TextField name='description' label='Description' multiline rows={3} defaultValue={activity?.description}/>
-            <TextField name='category' label='Category' defaultValue={activity?.category} />
-            <TextField name='date' label='Date' type='date' 
-              defaultValue={activity?.date ? new Date(activity.date).toISOString().split('T')[0] : new Date().toISOString().split('T')[0]} />
-            <TextField name='city' label='City' defaultValue={activity?.city} />
-            <TextField name='venue' label='Venue' defaultValue={activity?.venue} />
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} display="flex" flexDirection="column" gap={3}>
+            <TextInput label="Title" name='title' control={control} />
+            <TextInput label="Description" name='description' control={control} multiline rows={3} />
+            <Box display='flex' gap={3}>
+                <SelectInput items={categoryOptions} label="Category" name='category' control={control} />
+                <DateTimeInput label="Date" name='date' control={control} />
+            </Box>
+            <LocationInput label='Enter the location' name='location' control={control}></LocationInput>
             <Box display='flex' justifyContent='end' gap={3}>
                 <Button onClick={() => navigate("/activities")} color='inherit'>Cancel</Button>
                 <Button type='submit' color='success' variant='contained' disabled={updateActivity.isPending || createActivity.isPending}>Submit</Button>
---
+++ Client/src/lib/schemas/activitySchema.tsx
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+const requiredString = (fieldName: string) =>
+  z
+    .string({ error: `${fieldName} is required` })
+    .min(1, { message: `${fieldName} is required` });
+
+export const activitySchema = z.object({
+  title: requiredString("Title"),
+  description: requiredString("Description"),
+  date: z.preprocess(
+    (val) => {
+      if (val instanceof Date) return val; // <-- FIX
+      if (val === "") return undefined;
+      return val;
+    },
+    z.coerce.date({
+      error: "Date is required", // custom message
+    })
+  ),
+
+  category: requiredString("Category"),
+  location: z.object({
+    venue: requiredString("Venue"),
+    city: z.string().optional(),
+    latitude: z.coerce.number(),
+    longitude: z.coerce.number()
+  })
+});
+
+export type ActivitySchema = z.input<typeof activitySchema>;
