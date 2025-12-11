21f29b427e7242b89f647e941a72017321bd279f
Hassan <h.jamil1713@gmail.com>
2025-12-11 21:46:44 +0300

    End of Section 20

--- Client/src/features/profiles/ProfileFollowings.tsx
+++ Client/src/features/profiles/ProfileFollowings.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import { useProfile } from "../../lib/hooks/useProfile";
 import { Box, Divider, Typography } from "@mui/material";
 import ProfileCard from "./ProfileCard";
+import { useAccount } from "../../lib/hooks/useAccount";
 
 type Props = {
   activeTab: number;
@@ -11,6 +12,7 @@ export default function ProfileFollowings({ activeTab }: Props) {
   const { id } = useParams();
   const predicate = activeTab === 3 ? "followers" : "followings";
   const { profile, followings, loadingFollowings } = useProfile(id, predicate);
+  const { currentUser } = useAccount();
 
   return (
     <Box>
@@ -22,12 +24,24 @@ export default function ProfileFollowings({ activeTab }: Props) {
         </Typography>
       </Box>
       <Divider sx={{ my: 2 }} />
-      {loadingFollowings ? <Typography>Loading...</Typography> : (
-        <Box display='flex' marginTop={3} gap={3}>
-            {followings?.map(profile => (
-                <ProfileCard key={profile.id} profile={profile} />
-            ))}
+      {loadingFollowings ? (
+        <Typography>Loading...</Typography>
+      ) : followings !== undefined && followings?.length > 0 ? (
+        <Box display="flex" marginTop={3} gap={3}>
+          {followings?.map((profile) => (
+            <ProfileCard key={profile.id} profile={profile} />
+          ))}
         </Box>
+      ) : (
+        <Typography>
+          {activeTab === 3 && profile?.id !== currentUser?.id
+            ? `${profile?.displayName} does not have any followers`
+            : activeTab === 3 && profile?.id === currentUser?.id
+            ? `You do not have any followers`
+            : activeTab !== 3 && profile?.id !== currentUser?.id
+            ? `${profile?.displayName} is not following anyone`
+            : "You are not following anyone"}
+        </Typography>
       )}
     </Box>
   );
