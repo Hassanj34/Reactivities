4ca5ea0741a0f90c2c2a76768f814a966806ea1d
Hassan <h.jamil1713@gmail.com>
2025-12-14 14:50:44 +0300

    Email confirmation complete

--- Client/src/features/account/RegisterForm.tsx
+++ Client/src/features/account/RegisterForm.tsx
@@ -1,52 +1,86 @@
-import { useAccount } from '../../lib/hooks/useAccount'
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Box, Button, Paper, Typography } from '@mui/material';
-import { LockOpen } from '@mui/icons-material';
-import TextInput from '../../app/shared/components/TextInput';
-import { Link } from 'react-router';
-import { registerSchema, type RegisterSchema } from '../../lib/schemas/registerSchema';
+import { useAccount } from "../../lib/hooks/useAccount";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { LockOpen } from "@mui/icons-material";
+import TextInput from "../../app/shared/components/TextInput";
+import { Link } from "react-router";
+import {
+  registerSchema,
+  type RegisterSchema,
+} from "../../lib/schemas/registerSchema";
+import { useState } from "react";
+import RegisterSuccess from "./RegisterSuccess";
 
 export default function RegisterForm() {
+  const { registerUser } = useAccount();
+  const [registerSuccess, setRegisterSuccess] = useState(false);
 
-    const {registerUser} = useAccount();
-    
-    const {control, handleSubmit, setError, formState: { isValid, isSubmitting }} = useForm<RegisterSchema>({
-        mode: 'onTouched',
-        resolver: zodResolver(registerSchema)
+  const {
+    control,
+    handleSubmit,
+    watch,
+    setError,
+    formState: { isValid, isSubmitting },
+  } = useForm<RegisterSchema>({
+    mode: "onTouched",
+    resolver: zodResolver(registerSchema),
+  });
+
+  // eslint-disable-next-line react-hooks/incompatible-library
+  const email = watch("email");
+
+  const onSubmit = async (data: RegisterSchema) => {
+    await registerUser.mutateAsync(data, {
+      onSuccess: () => {
+        setRegisterSuccess(true);
+      },
+      onError: (error) => {
+        if (Array.isArray(error)) {
+          error.forEach((err) => {
+            if (err.includes("Email")) setError("email", { message: err });
+            else if (err.includes("Password"))
+              setError("password", { message: err });
+          });
+        }
+      },
     });
-
-    const onSubmit = async (data: RegisterSchema) => {
-        await registerUser.mutateAsync(data, {
-            onError: (error) => {
-                if(Array.isArray(error)){
-                    error.forEach(err => {
-                        if(err.includes('Email')) setError('email', {message: err});
-                        else if(err.includes('Password')) setError('password', {message: err});
-                    });
-                }
-            }
-        });
-    }
+  };
 
   return (
-    <Paper 
-        component='form' 
-        onSubmit={handleSubmit(onSubmit)}
-        sx={{display: 'flex', flexDirection: 'column', p: 3, gap: 3, maxWidth: 'md', mx: 'auto', borderRadius: 3}}
-    > 
-        <Box display='flex' alignItems='center' justifyContent='center' gap={3} color='secondary.main'>
-            <LockOpen fontSize='large' />
-            <Typography variant='h4'>Register</Typography>
-        </Box>
-        <TextInput label='Email' control={control} name='email' />
-        <TextInput label='Display Name' control={control} name='displayName' />
-        <TextInput label='Password' type='password' control={control} name='password' />
-        <Button type='submit' disabled={!isValid || isSubmitting} variant='contained' size='large'>Register</Button>
-        <Typography sx={{textAlign:'center'}}>
+    <>
+      {registerSuccess ? (
+        <RegisterSuccess email={email} />
+      ) : (
+        <Paper component="form" onSubmit={handleSubmit(onSubmit)}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            p: 3,
+            gap: 3,
+            maxWidth: "md",
+            mx: "auto",
+            borderRadius: 3,
+          }}
+        >
+          <Box display="flex" alignItems="center" justifyContent="center" gap={3} color="secondary.main">
+            <LockOpen fontSize="large" />
+            <Typography variant="h4">Register</Typography>
+          </Box>
+          <TextInput label="Email" control={control} name="email" />
+          <TextInput label="Display Name" control={control} name="displayName" />
+          <TextInput label="Password" type="password" control={control} name="password" />
+          <Button type="submit" disabled={!isValid || isSubmitting} variant="contained" size="large">
+            Register
+          </Button>
+          <Typography sx={{ textAlign: "center" }}>
             Already have an account?
-            <Typography sx={{ml: 1}} component={Link} to='/login' color='primary'>Sign In</Typography>
+            <Typography sx={{ ml: 1 }} component={Link} to="/login" color="primary">
+              Sign In
             </Typography>
-    </Paper>
-  )
+          </Typography>
+        </Paper>
+      )}
+    </>
+  );
 }
---
+++ Client/src/features/account/RegisterSuccess.tsx
@@ -0,0 +1,31 @@
+import { useAccount } from '../../lib/hooks/useAccount'
+import { Button, Paper, Typography } from '@mui/material';
+import { Check } from '@mui/icons-material';
+
+type Props = {
+    email?: string
+}
+
+export default function RegisterSuccess({email} : Props) {
+
+    const {resendConfirmationEmail} = useAccount();
+
+    if(!email) return null;
+  return (
+    <Paper 
+        sx={{
+            height: 400,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            p: 6
+        }}
+    >
+        <Check sx={{fontSize: 100, color: 'primary'}} />
+        <Typography variant='h3' gutterBottom>You have successfully registered!</Typography>
+        <Typography variant='h3' gutterBottom>Please check your email to confirm your account</Typography>
+        <Button fullWidth onClick={() => resendConfirmationEmail.mutate({email})}>Re-send confirmation email</Button>
+    </Paper>
+  )
+}
